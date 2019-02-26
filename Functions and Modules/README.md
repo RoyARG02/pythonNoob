9fe62de57603f4e61ec0bcfee6151f2532d7846a
RoyARG02 <anuragr9847@gmail.com>
2019-02-26 17:01:15 +0530

    Remove duplicated directory

--- Functions and Modules/README.md
+++
@@ -1,7 +0,0 @@
-Functions and Modules
-  1. Functions
-  2. Variable Arguments
-  3. Returning Multiple values
-  4. Lambda Functions
-  5. Default Arguments
-  6. map(), filter(), reduce()
--- Functions and Modules/lambdaFunctions.py
+++
@@ -1,8 +0,0 @@
-#lambda functions
-'''similar to anonymous functions that can take multiple
-arguments but returns single value'''
-#lambda <arguments>:<return value>
-sum=lambda x,y: x+y
-a=int(input('Enter a number: '))
-b=int(input('Enter another number: '))
-print('Sum of %d and %d is %d'%(a,b,sum(a,b)))
