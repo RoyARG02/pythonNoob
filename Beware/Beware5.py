27d82a84c4ec90d029893a9888f4f0e0fa01852c
RoyARG02 <anuragr9847@gmail.com>
2019-04-28 14:52:16 +0530

    My whole life has been a lie （　ﾟДﾟ）

---
+++ Beware/Beware5.py
@@ -0,0 +1,5 @@
+#python only supports single line comments, LIKE THIS ONE
+'''the multiple line comments are actually a string literal spanning multiple lines
+without any reference. This string object will be immediately deleted after the 
+interpreter has executed it. It leaves on effect on the rest of the code, thus
+functioning like a comment'''
